beaa3fe8ef5f2bdae164b5cb5a3d2875295fc9a4
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-25 17:50:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const logDriverNames = function (driver) {
+  driver.forEach(x => console.log(x.name));
+}
+
+const logDriversByHometown = function (driver, location) {
+  driver.forEach(x => {if (x.hometown === location) {
+    console.log(x.name)} })
+}
+
+const driversByRevenue = function (driver){
+  const newDriver = driver.sort(function(a, b){return a.revenue - b.revenue})
+  return newDriver;
+}
