8201de2affb1a29391d84ef667517ee347bfed57
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-25 17:56:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const logDriversByHometown = function (driver, location) {
 }
 
 const driversByRevenue = function (driver){
+  const newDriver = driver
  const newDriver =  driver.sort(function(a, b){ return a.revenue - b.revenue})
  return newDriver;
  console.log(driver)
